4b1ec17b7485e4c6b1894574989fd6dd294d6f5a
- <->
2025-03-31 22:46:48 +0300

    globals

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import { Logo } from '../../components/logo/Logo';
 import Menu from '../../components/menu/Menu';
+import { Container } from '../../components/container/Container';
+import { FlexWrapper } from '../../components/flexWrapper/FlexWrapper';
 
 const headerItems: string[] = [
   'Home',
@@ -13,15 +15,16 @@ const headerItems: string[] = [
 export const Header = () => {
   return (
     <StyledHeader>
-      <Logo />
-      <Menu menuItems={headerItems} />
+      <Container>
+        <FlexWrapper justify={'space-between'} align={'center'}>
+          <Logo />
+          <Menu menuItems={headerItems} />
+        </FlexWrapper>
+      </Container>
     </StyledHeader>
   );
 };
 
 const StyledHeader = styled.header`
   background-color: #d4ffd3;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
 `;
